223c2eebe4eeaab1d57b6b3129b15e6954a92c0f
kMercu <kamilochacon7@gmail.com>
2024-04-22 12:18:51 -0500

    Actualice el objeto y las variables

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -1,5 +1,47 @@
 package com.mycompany.libropoo;
 
 public class Libro {
+    private String title;
+    private String genre;
+    private String author;
+    private int yearOfPublication;
+    private boolean read;
+
+    // Constructor de la clase
+    public Libro(String title, String author, int yearOfPublication, String genre) {
+        this.title = title;
+        this.genre = genre;
+        this.author = author;
+        this.yearOfPublication = yearOfPublication;
+        this.read = false;
+    }
+
+    /**
+     * Complejidad temporal: O(1) - Tiempo constante.
+     */
+    public String mostrarInformacion() {
+        String impr = "Título: " + title + ", Autor: " + author + ", Año de publicación: " 
+                + yearOfPublication + ",Genero:" + genre + "Leído: " + (read ? "Sí" : "No");
+        return impr;
+    }
+
     
+    public void marcarLeido() {
+        this.read = true;
+    }
+    
+    public boolean getLeido(){
+        return read;
+    }
+    
+    public boolean esAntiguo() {
+        if (yearOfPublication<1974){
+        return true;
+        } else {
+        return false;
+        }
+        
+        
+    }
+
 }
--- src/main/java/com/mycompany/libropoo/Principal.java
+++ src/main/java/com/mycompany/libropoo/Principal.java
@@ -1,5 +1,28 @@
 package com.mycompany.libropoo;
 
+import java.util.Scanner;
+
 public class Principal {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese el título del libro:");
+        String title = sc.nextLine();
+
+        System.out.println("Ingrese el autor del libro:");
+        String author = sc.nextLine();
+
+        System.out.println("Ingrese el año de publicación del libro:");
+        int yearOfPublication = sc.nextInt();
+
+
+        Libro libro = new Libro(title, author, yearOfPublication);
+            libro.marcarLeido();
+
+        System.out.println(libro.mostrarInformacion());
+        System.out.println("¿El libro ha sido leído? " + (libro.getLeido() ? "Sí" : "No"));
+        System.out.println("¿El libro es antiguo? " + (libro.esAntiguo() ? "Sí" : "No"));
+
+        sc.close();
+    }
 }
